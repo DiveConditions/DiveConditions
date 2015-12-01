7d64044ba105b5e0772bc7e404361a9dbbc5d858
Marc Dürst <marc@idev.ch>
2015-12-01 16:12:01 +0100

    Added ICommand interface

---
+++ src/DiveConditions.Infrastructure/Messaging/ICommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiveConditions.Infrastructure.Command
+{
+    public interface ICommand
+    {
+        /// <summary>
+        /// Gets the command identifier.
+        /// </summary>
+        Guid Id { get; }
+    }
+}
